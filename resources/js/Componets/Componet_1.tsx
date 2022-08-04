7851e13c024e9aca465babadec78a341358f9473
vagrant <vagrant@homestead>
2022-08-04 22:05:45 +0000

    magamento toreac for app

---
+++ resources/js/Componets/Componet_1.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { up } from '../Untils/untils';
+
+const Componet_1 =()=>{
+  return (
+    <>
+    {up('componente')}</>
+  )
+}
+
+export default Componet_1;
---
+++ resources/js/Pages/Index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import Componet_1 from '../Componets/Componet_1'
+const  Index = ()=>{
+  return (
+    <><Componet_1></Componet_1></>
+  )
+}
+
+export default Index;
